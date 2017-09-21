65b5bd430e2d57c0dde92b6fe999b2b32b4c47be
hang <304035020@qq.com>
2017-09-21 23:49:51 +0800

    editpost

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from flask import current_app
 from datetime import datetime
 from flask import request
 import hashlib
+from markdown import markdown
+import bleach
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -195,6 +197,7 @@ class Post(db.Model):
 	body = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	body_html = db.Column(db.Text)
 
 	@staticmethod
 	def generate_fake(count=200):
@@ -210,6 +213,19 @@ class Post(db.Model):
 						author=u)
 			db.session.add(p)
 			db.session.commit()
+
+
+	@staticmethod
+	def on_changed_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'oldvalue', 'b', 'blockquote', 'code',
+						'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+						'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 
