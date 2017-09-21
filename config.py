43fcf53518644b8a8c343a08499823e52390354d
hang <304035020@qq.com>
2017-09-21 15:34:29 +0800

    change config

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class DevelopmentConfig(Config):
 	MAIL_PORT = 25
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = 'shanhaihang@163.com'
-	#dddd
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	MAIL_SENDER = 'shanhaihang@163.com'
 	FLASKY_ADMIN = '304035020@qq.com'
 	FLASKY_POSTS_PER_PAGE = 10
