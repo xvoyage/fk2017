5a93c876462047152cbd582b2ca95b1f603ec0b8
hang <304035020@qq.com>
2017-10-10 18:09:52 +0800

    20171010

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, session, redirect, url_for, flash,request, current_app
+from flask import render_template, session, redirect, url_for, flash,request, current_app, make_response
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
-from ..models import User, Permission, Role, Post,Follow
+from ..models import User, Permission, Role, Post,Follow, Comment
 from ..decorators import admin_required, permission_required
 from flask_login import login_required, current_user
 from flask import abort
@@ -15,14 +15,37 @@ def index():
 		post = Post(body=form.body.data, author=current_user._get_current_object())
 		db.session.add(post)
 		return redirect(url_for('.index'))
+
+	show_followed_posts = False
+	if current_user.is_authenticated:
+		show_followed_posts = bool(request.cookies.get('show_followed', ''))
+	if show_followed_posts:
+		query = current_user.followed_posts
+	else:
+		query = Post.query
+
 	page = request.args.get('page', 1, type=int)
-	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+	pagination = query.order_by(Post.timestamp.desc()).paginate(
 		page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
 		error_out=False)
 	posts = pagination.items
 	return render_template('index.html', form=form, posts=posts,
-					pagination=pagination)
+					pagination=pagination, show_followed=show_followed_posts)
 
+
+@main.route('/all')
+@login_required
+def show_all():
+	rep = make_response(redirect(url_for('.index')))
+	rep.set_cookie('show_followed', '', max_age=30*24*60*60)
+	return rep
+
+@main.route('/follower')
+@login_required
+def show_followed():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+	return resp
 
 @main.route('/admin')
 @login_required
@@ -91,7 +114,24 @@ def edit_profile_admin(id):
 @main.route('/post/<int:id>')
 def post(id):
 	post = Post.query.get_or_404(id)
-	return render_template('post.html', posts=[post])
+	form = CommentForm()
+	if form.validate_on_submit():
+		comment = comment(body=form.body.data,
+							post=post,
+							author=current_user._get_current_object())
+		db.session.add(comment)
+		flash('Your comment has been published.')
+		return redirect(url_for('.post', id=post.id, page=-1))
+	page = request.args.get('page', 1, type=int)
+	if page == -1:
+		page = (post.comments.count() -1 ) / \
+			current_app.config['FLASKY_POSTS_PER_PAGE'] + 1
+	pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+		page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+		error_out=False)
+	comments = pagination.items
+	return render_template('post.html', posts=[post], form=form,
+							comments=comments, pagination=pagination)
 
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
